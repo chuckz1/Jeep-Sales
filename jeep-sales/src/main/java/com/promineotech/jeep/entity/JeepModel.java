73f2cd1af37faf93a5f89fc75e7fa80f8c6a5e21
chuckz1 <fehringercharles9@gmail.com>
2023-03-31 16:56:21 -0600

    Initial Commit

---
+++ jeep-sales/src/main/java/com/promineotech/jeep/entity/JeepModel.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.promineotech.jeep.entity;
+
+/**
+ * @author charles
+ *
+ */
+public enum JeepModel {
+	GRAND_CHEROKEE,
+	CHEROKEE,
+	COMPASS,
+	RENEGADE,
+	WRANGLER,
+	GLADIATOR,
+	WRANGLER_4XE
+}
